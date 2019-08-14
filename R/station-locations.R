5b590eac01805d92fdc41dcf252c436c434ab651
katiejolly <katherine.jolly@metc.state.mn.us>
2019-08-14 11:40:08 -0500

    udpate station data

---
+++ R/station-locations.R
@@ -0,0 +1,27 @@
+#' MN niceride station locations
+#'
+#' Data from MN niceride in 2017 on station locations (lat/lon) as well as the total number of docks available.
+#'
+#'
+#' @docType data
+#'
+#' @usage data(station_locations)
+#'
+#' @format A data frame with 202 rows and 5 variables:
+#' \describe{
+#'   \item{number}{station id}
+#'   \item{name}{station name}
+#'   \item{latitude}{}
+#'   \item{longitude}{}
+#'   \item{total_docks}{number of bike docks at each station}
+#'   }
+#'
+#'
+#' @keywords datasets
+#'
+#' @references MN niceride https://www.niceridemn.com/system-data
+#'
+#' @source \href{https://www.kaggle.com/brendanhasz/nice-ride-mn-2017/downloads/nice-ride-mn-2017.zip/2}{Nice Ride Minnesota 2017}
+#'
+
+"station_locations"
